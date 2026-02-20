cced320042ae5d3c3a714282f66f5992f760b03e
Sid Baliga <siddarth1981@gmail.com>
2026-02-20 11:01:26 +0530

    Fix puppeteer launch config

--- server.js
+++ server.js
@@ -22,9 +22,13 @@ async function generatePDF(data) {
   });
 
   const browser = await puppeteer.launch({
-    headless: true,
-    args: ["--no-sandbox", "--disable-setuid-sandbox"]
-  });
+  headless: "new",
+  args: [
+    "--no-sandbox",
+    "--disable-setuid-sandbox",
+    "--disable-dev-shm-usage"
+  ]
+});
 
   const page = await browser.newPage();
   await page.setContent(html, { waitUntil: "networkidle0" });
